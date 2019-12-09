bf7d9dda9e5708ef68a75d251d8acb86bab2d789
Student <example@example.com>
2019-12-09 14:11:34 -0800

    pass test

--- spec/leet_speak_spec.rb
+++ spec/leet_speak_spec.rb
@@ -3,6 +3,6 @@ require('leet_speak')
 
 describe('leet_speak') do
   it("The letter e should be replaced with '3'") do
-    expect(leet_speak("e")).to(eq(["4"]))
+    expect(leet_speak("e")).to(eq(["3"]))
   end
 end
