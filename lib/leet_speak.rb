fefa99aaefcc41443b297c7960c890ab90b2254b
Student <example@example.com>
2019-12-09 15:04:35 -0800

    passes three texts and joins back to string

--- lib/leet_speak.rb
+++ lib/leet_speak.rb
@@ -1,16 +1,28 @@
 def leet_speak(words)
- array=[]
- split_sentence = words.split("")
- split_sentence.each do |letter|
+  array=[]
+  split_sentence = words.split("")
+  split_sentence.each do |letter|
     if (letter === "e")
       array.push("3")
     elsif (letter === "o")
       array.push("0")
     elsif (letter === "I")
       array.push("1")
-    # else
-    #   array.push(x)
+    else
+      array.push(letter)
     end
   end
-  array
+  array.join()
 end
+
+  # split_sentence.join("")
+  # split_sentence2 = split_sentence.split(" ")
+  # split_sentence2.each do |word|
+  #   if (word[0] === "s")
+  #     array.push("s")
+  #   elsif (word.includes("s"))
+  #     array.push("z")
+  #   else
+  #     array.push(word)
+  #   end
+  # end
--- spec/leet_speak_spec.rb
+++ spec/leet_speak_spec.rb
@@ -3,18 +3,24 @@ require('leet_speak')
 
 describe('leet_speak') do
   it("The letter e should be replaced with '3'") do
-    expect(leet_speak("e")).to(eq(["3"]))
+    expect(leet_speak("el")).to(eq("3l"))
   end
 end
 
 describe('leet_speak') do
   it("The letter o should be replaced with '0'") do
-    expect(leet_speak("o")).to(eq(["0"]))
+    expect(leet_speak("old")).to(eq("0ld"))
   end
 end
 
 describe('leet_speak') do
   it("The letter I should be replaced with '1'") do
-    expect(leet_speak("I")).to(eq(["1"]))
+    expect(leet_speak("I")).to(eq("1"))
   end
 end
+
+# describe('leet_speak') do
+#   it("The letter s should be replaced with 'z' except at the beginning of the word") do
+#     expect(leet_speak("sass")).to(eq(["sazz"]))
+#   end
+# end
